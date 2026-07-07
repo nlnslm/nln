fd49f1667291c459821751d6ea696711098cc19f
Nalinee <nlnslm012@gmail.com>
2026-07-07 11:44:35 +0700

    My first hello world

---
+++ 477-201(do not open)/6810510531_1.py
@@ -0,0 +1,15 @@
+#6810510531 Piyawat Jantharachota
+print(" /\ /\ ")
+print("((ovo)) ")
+print("():::()")
+print("  vvv  ")
+print("Hello User, i'm FAT OWL bot")
+name=input("what your name? :")
+print("HI " ,name)
+age=int(input("how old are you?"))
+if age > 60 :
+         print("WOW!! you have a lot of time to enjoy your journey")
+else :
+         print("enjoy your retire and peaceful life")
+print("you have to go?")
+print("fine. see you, take your time")
